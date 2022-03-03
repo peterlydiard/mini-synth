0b6da736322a428f0f768cdb8ccf9a8c58b86863
peterlydiard <peter_lydiard@yahoo.co.uk>
2022-03-03 21:34:52 +0000

    Adjust control inputs to VCF to approximate desired characteristics.

--- filter_test.py
+++ filter_test.py
@@ -5,12 +5,13 @@ import math
 import numpy as np
 import pygame
 
-from guizero import App, Window, Drawing, Box, PushButton, Combo
+from guizero import App, Window, Drawing, Box, PushButton, Combo, Slider, Text
 
 
 ######################### Global variables #########################
 
-FREQUENCY = 110    # Hz
+LOWEST_TONE = 110
+FREQUENCY = LOWEST_TONE    # Hz
 WIDTH = 50
 MAX_DURATION = 500
 STEREO = True
@@ -54,15 +55,34 @@ class View:
         self.panel_1.set_border(thickness=10, color=self.app.bg)
 
         self.filter_button = Combo(self.panel_1, grid=[0,0], options=["Lowpass", "Bandpass", "Highpass", "Notch"],
-                                     height="fill", command=self.handle_set_filter)
-        self.test_button = PushButton(self.panel_1, grid=[1,0], text="Test", command=self._handle_request_test)
+                                     height="fill", command=self._handle_set_filter)
+        Text(self.panel_1, grid=[1,0], text="Frequency control")
+        self.frequency_slider = Slider(self.panel_1, grid=[1,1], start=0, end=90, width=180, command=self._handle_set_frequency)
+        Text(self.panel_1, grid=[2,0], text="Filter Q factor")
+        self.q_factor_slider = Slider(self.panel_1, grid=[2,1], start=1, end=6, command=self._handle_set_q_factor)
+        self.test_button = PushButton(self.panel_1, grid=[3,0], text="Test", command=self._handle_request_test)
         
         # set up exit function
-        self.app.when_closed = self.handle_close_app
+        self.app.when_closed = self._handle_close_app
         
         # enter endless loop, waiting for user input.
         self.app.display()
 
+
+    def play_sound(self, wave):
+        if not self.sound is None:
+            self.sound.fadeout(50)
+            del self.sound
+        # Ensure that highest value is in 16-bit range
+        audio = wave * (2**15 - 1) / np.max(np.abs(wave))
+        # Convert to 16-bit data
+        audio = audio.astype(np.int16)
+        # create a pygame Sound object and play it.
+        sound = pygame.sndarray.make_sound(audio)
+        sound.play()
+        self.sound = sound
+        
+        
     def draw_grid(self, min_x, max_x, step_x, min_y, max_y, step_y):
         self._debug_2("Drawing a new graph")
 
@@ -137,7 +157,7 @@ class View:
             previous_y = plot_y
         
 
-    def handle_close_app(self):
+    def _handle_close_app(self):
         self._debug_1("\nNormal termination")
         pygame.mixer.music.stop()
         self.app.destroy()
@@ -152,10 +172,13 @@ class View:
         if debug_level >= 2:
             print(message)        
     
-    def handle_set_frequency(self, value):
+    def _handle_set_frequency(self, value):
         self.controller.on_request_frequency(value)
         
-    def handle_set_filter(self, value):
+    def _handle_set_q_factor(self, value):
+        self.controller.on_request_q_factor(value)
+        
+    def _handle_set_filter(self, value):
         self.controller.on_request_filter(value)
         
     def _handle_request_test(self):
@@ -163,7 +186,7 @@ class View:
         
         
 class Model:
-    def __init__(self, sample_rate, frequency=FREQUENCY, max_duration=MAX_DURATION):
+    def __init__(self, sample_rate, frequency=LOWEST_TONE, max_duration=MAX_DURATION):
         self.sample_rate = sample_rate     # Hz
         self.frequency = frequency         # Hz
         self.max_duration = max_duration   # milliseconds
@@ -181,12 +204,20 @@ class Model:
         tone = np.sin(radians_per_msec * times_msec)
         return tone
 
-    
-    def voltage_controlled_filters(self, tone, control):
+    # VCF - Voltage Controlled Filters.
+    # tone = input waveform to be filtered
+    # freq_control = control signal to set filter centre frequency for bandpass and notch filters, or
+    # cutoff frequency for lowpass and highpass filters. Range = 0 to about 7.5, with one octave per unit
+    # starting with 0 -> lowest keyboard tone and 
+    def voltage_controlled_filters(self, tone, freq_control, q_factor):
         
         # Truncate raw tone to length of control waveform
-        tone = tone[:len(control)]
+        tone = tone[:len(freq_control)]
         
+        # Calculate low pass filter coefficients (time-varying).
+        alpha = np.zeros(len(freq_control))
+        for i in range(len(freq_control)):
+            alpha[i] = min(1.0, math.pow(freq_control[i], 1.5) / 16.5)
         #self._debug_2("VCF signal length = " + str(len(tone)))
         
         # Clear stores for HP and LP outputs.
@@ -194,38 +225,36 @@ class Model:
         low_pass = np.zeros(len(tone) + 2)
         x0 = x1 = x2 = x3 = x4 = 0
         
-        for i in range(len(tone)):
-            
-            x0 = (control[i] * tone[i]) + ((1.0 - control[i]) * x1) 
-            x2 = (control[i] * x1) + ((1.0 - control[i]) * x3)
-            
+        for i in range(len(tone)-2):
+            x0 = (alpha[i] * tone[i]) + ((1.0 - alpha[i]) * x1) 
+            x2 = (alpha[i] * x1) + ((1.0 - alpha[i]) * x3)
             low_pass[i] = x3
-            high_pass[i] = tone[i] - x3
-            
+            high_pass[i] = tone[i] - x2
             x3 = x2
             x1 = x0
         
+        # Bandpass and bandstop (notch) biquadratic filters.
         band_pass = np.zeros(len(tone) + 2)
         notch = np.zeros(len(tone) + 2)
         x0 = x1 = x2 = x3 = x4 = 0
-        
-        fc = 220
-        Rn = 0.975
-        R = 1 - ((1 - Rn) *  fc * 64 / SAMPLE_RATE)
-        b2 = - R
-        a1 = - 2 * R * np.cos(2 * np.pi * fc / SAMPLE_RATE)
-        a2 = R * R
-        # self._debug_2("BP filter a1, a2 = " + str(a1) + ", " + str(a2))
-
-        rescale = 1 - R
         for i in range(len(tone)):
+            # Recalculate filter coefficients, every 1.45 milliseconds.
+            if i % 64 == 0:
+                fc = min(0.4 * SAMPLE_RATE, (LOWEST_TONE * np.power(2, (freq_control[i]))) + 0.5)
+                R = 1 - (np.pi * fc / (q_factor * SAMPLE_RATE))
+                b2 = - R
+                a1 = - 2 * R * np.cos(2 * np.pi * fc / SAMPLE_RATE)
+                a2 = R * R            
+                rescale = 1 - R
+            
+            # Calculate biquad filter.
             x0 = tone[i] - (a1 * x1) - (a2 * x2)
             band_pass[i] = rescale * (x0 + (b2 * x2))
-            
+            notch[i] = tone[i] - band_pass[i]
             x2 = x1
             x1 = x0
         
-        return low_pass[2:], band_pass[2:], high_pass[2:]
+        return low_pass[2:], high_pass[2:], band_pass[2:], notch[2:]
 
     def _debug_1(self, message):
         global debug_level
@@ -245,41 +274,51 @@ if __name__ == "__main__":
 
     SAMPLE_RATE = 44100
     DURATION = 200
-    FREQUENCY = 55
     
     class TestController:
         def __init__(self):
             self.sample_rate = SAMPLE_RATE
             self.waveform = "Sine"
             self.filter = "Lowpass"
+            self.frequency_control = 0.0
+            self.q_factor = 1.0
             self.frequency = 55
             self.view = View(self)
             self.model = Model(SAMPLE_RATE, max_duration=DURATION)
         
         def main(self):
-            self.view._debug_2("In main of test controller")
+            self.view._debug_1("In main of test controller")
             self.view.main()
            
         def on_request_frequency(self, frequency):
-            self.view._debug_2("Set frequency to " + str(frequency))
+            self.view._debug_1("Set frequency control to " + str(float(frequency)/12.0))
+            self.frequency_control = float(frequency) / 12.0
                       
+        def on_request_q_factor(self, q_factor):
+            self.view._debug_1("Set Q factor to " + str(q_factor))
+            self.q_factor = float(q_factor)
+            
         def on_request_filter(self, filter):
-            self.view._debug_2("Set filter to " + str(filter))
+            self.view._debug_1("Set filter to " + str(filter))
             self.filter = filter
             
         def on_request_test(self):
-            self.view._debug_2("Test requested")
+            self.view._debug_1("Test requested")
             
-            num_tones = 94
+            num_tones = int(12 * math.log2(0.5 * SAMPLE_RATE / LOWEST_TONE))
+            print("Number of semitones = " + str(num_tones))
+            
             lp_gain = np.zeros(num_tones)
-            bp_gain = np.zeros(num_tones)
             hp_gain = np.zeros(num_tones)
+            bp_gain = np.zeros(num_tones)
+            bs_gain = np.zeros(num_tones)
+            
             cutoff_freq = 0
             cutoff_gain = 1.0
             
             # Generate a range of tones and pass them through the filter function
             for semitone in range(num_tones):
-                frequency = int((110 * np.power(2, semitone/12)) + 0.5)
+                frequency = int((LOWEST_TONE * np.power(2, semitone/12)) + 0.5)
                 
                 #frequency = 18350 + semitone
                 
@@ -292,20 +331,23 @@ if __name__ == "__main__":
             
                 #print("Sine wave, number of samples = ", len(sine_tone))
     
-                control = np.ones(len(sine_tone)) * 0.2
+                control = np.ones(len(sine_tone)) * (self.frequency_control + 0.001)
             
-                lp, bp, hp =  self.model.voltage_controlled_filters(sine_tone, control)
+                lp, hp, bp, bs =  self.model.voltage_controlled_filters(sine_tone, control, self.q_factor)
             
                 lp_range = np.nanmax(lp) - np.nanmin(lp)
-                bp_range = np.nanmax(bp) - np.nanmin(bp)
                 hp_range = np.nanmax(hp) - np.nanmin(hp)
+                bp_range = np.nanmax(bp) - np.nanmin(bp)
+                bs_range = np.nanmax(bs) - np.nanmin(bs)
                 
                 if lp_range <= 0:
                     print("WARNING: semitone, lp_range = " + str(semitone) + ", " + str(lp_range))
-                if bp_range <= 0:
-                    print("WARNING: semitone, bp_range = " + str(semitone) + ", " + str(bp_range))
                 if hp_range <= 0:
                     print("WARNING: semitone, hp_range = " + str(semitone) + ", " + str(hp_range))
+                if bp_range <= 0:
+                    print("WARNING: semitone, bp_range = " + str(semitone) + ", " + str(bp_range))
+                if bs_range <= 0:
+                    print("WARNING: semitone, bs_range = " + str(semitone) + ", " + str(bs_range))
                     
                     
                 if lp_range > 0.8 and lp_range < 1.414:
@@ -316,20 +358,32 @@ if __name__ == "__main__":
                 
                 # Convert filter gain to decibels
                 lp_gain[semitone] = 20 * math.log10(lp_range / 2)
-                bp_gain[semitone] = 20 * math.log10(bp_range / 2)
                 hp_gain[semitone] = 20 * math.log10(hp_range / 2)
+                bp_gain[semitone] = 20 * math.log10(bp_range / 2)
+                bs_gain[semitone] = 20 * math.log10(bs_range / 2)
                 
-                audio = lp * 30000
-                audio = audio.astype(np.int16)
-                # create a pygame Sound object and play it.
-                sound = pygame.sndarray.make_sound(audio)
-                sound.play()
+                if bp_gain[semitone] > -5:
+                    print("Bandpass frequency, gain = " + str(frequency) + ", " + str(bp_gain[semitone]))
                 
+                if self.filter == "Lowpass":
+                    self.view.play_sound(sine_tone)
+                    print("Filter = Lowpass")
+                elif self.filter == "Highpass":
+                    self.view.play_sound(hp)
+                    print("Filter = Highpass")
+                elif self.filter == "Bandpass":
+                    self.view.play_sound(bp)
+                    print("Filter = Bandpass")
+                elif self.filter == "Notch":
+                    self.view.play_sound(bs)
+                    print("Filter = Notch")
+                    
             #self.view.show_graph(gain)
             self.view.prepare_graph()
             self.view.draw_graph(lp_gain, "light green")
             self.view.draw_graph(bp_gain, "orange")
             self.view.draw_graph(hp_gain, "red")
+            self.view.draw_graph(bs_gain, "blue")
             
             print("Lowpass graph minimum = " + str(np.nanmin(lp_gain)))
             print("Lowpass graph maximum = " + str(np.nanmax(lp_gain)))
@@ -349,10 +403,10 @@ if __name__ == "__main__":
             #print("Signs = " + str(op_max_sign) + ", " + str(op_min_sign))
             
         def on_request_save(self):
-            self.view._debug_2("Save settings requested")
+            self.view._debug_1("Save settings requested")
             
         def on_request_restore(self):
-            self.view._debug_2("Restore settings requested")
+            self.view._debug_1("Restore settings requested")
             
 
     debug_level = 2
